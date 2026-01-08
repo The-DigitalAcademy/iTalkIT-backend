b49d383a51ebc2c23c91f1fc3eb2c7d6fd2aad32
OfentseMolefe <molefeofentse@hotmail.com>
2026-01-08 11:36:07 +0200

    Initial commit

---
+++ italkit/src/main/java/com/italkit/italkit/ItalkitApplication.java
@@ -0,0 +1,13 @@
+package com.italkit.italkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItalkitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItalkitApplication.class, args);
+	}
+
+}
---
+++ italkit/src/test/java/com/italkit/italkit/ItalkitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.italkit.italkit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItalkitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
