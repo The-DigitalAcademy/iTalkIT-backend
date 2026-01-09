7256bc3e32e00793fbeede858b3572227f9438ff
Musa Gumede <gracegumedem@gmail.com>
2026-01-09 11:32:29 +0200

    added a data seed config, removed followers table replaced it by association table

---
+++ italkit/src/main/java/com/italkit/italkit/models/Post.java
@@ -0,0 +1,36 @@
+package com.italkit.italkit.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "posts")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 500)
+    private String content;
+
+    private String imageUrl;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    private int likesCount = 0;
+
+    private int commentsCount = 0;
+}
